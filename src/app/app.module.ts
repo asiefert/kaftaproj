0efc8a557461ac12110efd0f2f1095e59aea2d3a
Arin Siefert <arins123@outlook.com>
2021-02-21 02:38:17 -0500

    Finished Homepage, styled navbar, created footer

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,12 +5,16 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { NavbarComponent } from './navbar/navbar.component';
 import { SignUpComponent } from './sign-up/sign-up.component';
+import { HomeComponent } from './home/home.component';
+import { FooterComponent } from './footer/footer.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     NavbarComponent,
-    SignUpComponent
+    SignUpComponent,
+    HomeComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule,
--- src/app/navbar/navbar.component.ts
+++ src/app/navbar/navbar.component.ts
@@ -6,7 +6,7 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./navbar.component.css']
 })
 export class NavbarComponent implements OnInit {
-  title = 'Project Kafta';
+  title = 'Kafta';
   constructor() { }
 
   ngOnInit(): void {
